edd793a68052d3008f5571c492919b5e1ee9fc2f
Santanu Bhattacharyya <santanu@Santanus-MacBook-Pro.local>
2025-07-20 10:55:35 +0530

    Modules

---
+++ Ch05-Modules/01-import.py
@@ -0,0 +1,17 @@
+import math
+
+print(f'Value of E = {math.e}')
+
+
+from math import sqrt, pi
+
+result = sqrt(9) * pi
+print(result)
+
+from math import *
+print(f'Value of E = {e}')
+
+import math as m
+print(f'Value of E = {m.e}')
+
+print(f'All functions from Math: {dir(math)}')
---
+++ Ch05-Modules/02-my-module-app.py
@@ -0,0 +1,8 @@
+from my_module import result, welcome
+
+welcome()
+
+print(f'Result: {result}')
+
+from my_module import welcome as greet
+greet()
